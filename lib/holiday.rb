08b91969bd0152fec1ddaedcaa281c65ce6163ec
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-19 22:26:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.to_s.capitalize!}:"
     season_holidays_hash.each do |holiday_name, supplies|
 
-      puts "  #{holiday_name.to_s.capitalize!}: #{supplies.to_s.join(", ")}"
+      puts "  #{holiday_name.to_s.capitalize!}: #{supplies.join(", ")}"
     end
   end
 
